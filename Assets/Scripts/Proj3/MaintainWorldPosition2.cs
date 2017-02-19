3d01a39ab93c2288393d8b869b69e096213c4d53
Ty Stahnke <pstahnke@ucsd.edu>
2017-02-18 19:31:39 -0800

    fixed some errors.

---
+++ Assets/Scripts/Proj3/MaintainWorldPosition2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintainLocalPosition2 : MonoBehaviour {
+
+    private float x;
+    private float y;
+    private float z;
+
+    public float xPos = 0;
+    public float yPos = 0;
+    public float zPos = 0;
+
+    public bool xLock = false;
+    public bool yLock = false;
+    public bool zLock = false;
+
+    private bool selected = false;
+
+    private void Start()
+    {
+        xPos = gameObject.transform.localPosition.x;
+        yPos = gameObject.transform.localPosition.y;
+        zPos = gameObject.transform.localPosition.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        x = gameObject.transform.localPosition.x;
+        y = gameObject.transform.localPosition.y;
+        z = gameObject.transform.localPosition.z;
+
+        if (xLock) x = xPos;
+        if (yLock) y = yPos;
+        if (zLock) z = zPos;
+
+        gameObject.transform.localPosition = new Vector3(x, y, z);
+
+	}
+
+    public void SendSelected(bool selected)
+    {
+        this.selected = selected;
+    }
+}
---
+++ Assets/Scripts/Proj3/MaintainWorldRotation2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintainWorldRotation2 : MonoBehaviour {
+
+    private float x;
+    private float y;
+    private float z;
+
+    public float xRot = 0;
+    public float yRot = 0;
+    public float zRot = 0;
+
+    public bool xLock = false;
+    public bool yLock = false;
+    public bool zLock = false;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        x = gameObject.transform.eulerAngles.x;
+        y = gameObject.transform.eulerAngles.y;
+        z = gameObject.transform.eulerAngles.z;
+
+        if (xLock) x = xRot;
+        if (yLock) y = yRot;
+        if (zLock) z = zRot;
+
+        gameObject.transform.rotation = Quaternion.Euler(new Vector3(x,y,z));
+	}
+}
